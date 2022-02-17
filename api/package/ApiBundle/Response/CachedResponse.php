d7f570a4d7cef352e7817f94eba7d954e439f8ac
Ramazan APAYDIN <ramazan.apaydin@macellan.net>
2022-02-17 16:59:55 +0300

    Cache Response Update

--- api/package/ApiBundle/Response/CachedResponse.php
+++ api/package/ApiBundle/Response/CachedResponse.php
@@ -3,23 +3,29 @@
 namespace Package\ApiBundle\Response;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 
 /**
  * Proxy Cache Response.
  */
 class CachedResponse extends JsonResponse
 {
-    public static function create(array $data, int $ageSecond = 60): CachedResponse
+    public static function create(array $data, int $ageSecond = 60, ?array $tags = null): CachedResponse
     {
         $response = new self($data, 200, []);
 
-        // Disable Browser & Enable Proxy Cache Header
-        $response->setCache([
-            'no-cache' => true,
-            'public' => true,
-            'max_age' => $ageSecond,
-            's-maxage' => $ageSecond,
-        ]);
+        // Enable Session Cache
+        $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+        // Set Proxy Cache Timeout
+        $response->setMaxAge($ageSecond)->setSharedMaxAge($ageSecond);
+
+        // Add Cache Tags
+        if ($tags) {
+            $response->headers->set('Cache-Tag', implode(',', array_map(static function ($tag) {
+                return hash('crc32b', (string) $tag);
+            }, $tags)));
+        }
 
         return $response;
     }
