2f7fa93e0574f21651e81a6d9b17cc5bb4edd3d1
Ramazan APAYDIN <apaydin541@gmail.com>
2022-05-13 02:33:24 +0300

    TaskTesk Init

--- api/package/SwooleBundle/Test/TaskTest.php
+++ api/package/SwooleBundle/Test/TaskTest.php
@@ -2,13 +2,37 @@
 
 namespace Package\SwooleBundle\Test;
 
+use Package\SwooleBundle\Task\TaskWorker;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskTest extends WebTestCase
 {
     public function testTaskHandler(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/');
+        $container = static::bootKernel()->getContainer();
+        $this->assertTrue($container->has(TaskWorker::class));
+
+        /** @var TaskWorker $worker */
+        $worker = $container->get(TaskWorker::class);
+        $worker->handle([
+            'class' => 'TestTaskClass',
+            'data' => [],
+        ]);
+    }
+
+    public function testFailedCreate(): void
+    {
+    }
+
+    public function testFailedRetry(): void
+    {
+    }
+
+    public function testFailedClear(): void
+    {
+    }
+
+    public function testFailedView(): void
+    {
     }
 }
