1e2a9894330c7fe5d086f412b4abc72584865ae7
Ramazan APAYDIN <apaydin541@gmail.com>
2022-05-14 13:21:20 +0300

    Task Test Init with Mocked

--- api/package/SwooleBundle/Test/TaskTest.php
+++ api/package/SwooleBundle/Test/TaskTest.php
@@ -2,18 +2,39 @@
 
 namespace Package\SwooleBundle\Test;
 
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\Persistence\ObjectManager;
+use Package\SwooleBundle\Log\Logger;
 use Package\SwooleBundle\Task\TaskWorker;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class TaskTest extends WebTestCase
 {
+    private ObjectManager $entityManager;
+
+    protected function setUp(): void
+    {
+    }
+
     public function testTaskHandler(): void
     {
-        $container = static::bootKernel()->getContainer();
+        $client = self::createClient();
+
+        // Mock Logger
+        $logger = $this->getMockBuilder(Logger::class)->getMock();
+        $container = $client->getContainer();
+        $container->set('logger', $logger);
+
+        // Test Worker
         $this->assertTrue($container->has(TaskWorker::class));
+
+        // Init DB
+        $this->initDatabase($client->getKernel());
 
         /** @var TaskWorker $worker */
         $worker = $container->get(TaskWorker::class);
+        $logger->expects($this->once())->method('critical');
         $worker->handle([
             'class' => 'TestTaskClass',
             'data' => [],
@@ -34,5 +55,17 @@ class TaskTest extends WebTestCase
 
     public function testFailedView(): void
     {
+    }
+
+    private function initDatabase(KernelInterface $kernel): void
+    {
+        if ('test' !== $kernel->getEnvironment()) {
+            throw new \LogicException('Execution only in Test environment possible!');
+        }
+
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+        $metaData = $this->entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($this->entityManager);
+        $schemaTool->updateSchema($metaData);
     }
 }
