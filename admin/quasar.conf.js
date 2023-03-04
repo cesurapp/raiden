8b64b4ebda9f5d2c1a7ad18fb1b12eed593da9fc
Ramazan APAYDIN <apaydin541@gmail.com>
2023-03-04 22:59:45 +0300

    Change Dist Dir.

--- admin/quasar.conf.js
+++ admin/quasar.conf.js
@@ -81,7 +81,7 @@ module.exports = configure(function (ctx) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: 'dist',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
